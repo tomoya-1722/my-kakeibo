ff0e58ce649d5956123e895de6b29d6e907ad6ac
tomoya-1722 <tomoya.mura@icloud.com>
2026-02-20 04:03:35 +0900

    feat: restore PWA configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,20 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig);
